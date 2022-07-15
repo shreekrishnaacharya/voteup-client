d5ff1aa60ab2fd1881181ad0b88d9e507883526b
shreekrishnaacharya <sksharma72000@gmail.com>
2022-07-15 22:33:43 +0545

    bug fixed

--- src/view/main/pages/Pricing.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default function Pricing() {
-    console.log("Pricing");
-    return (
-        <div className="">
-            <h2>Pricing</h2>
-        </div>
-    );
-}
--- src/view/main/pages/Testimonials.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default function Testimonials() {
-    console.log("Testimonials");
-    return (
-        <div className="">
-            <h2>Testimonials</h2>
-        </div>
-    );
-}
