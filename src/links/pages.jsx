b17fb09346fc21cb7a9682003efb652c734ae9a4
shreekrishnaacharya <sksharma72000@gmail.com>
2022-11-12 21:58:27 +0545

    new update for mobile design view

--- src/links/pages.jsx
+++ src/links/pages.jsx
@@ -26,7 +26,7 @@ const otherPages = {
 const pages = {
     // BASE_URL: "http://ventvoila.com/api",
     // BASE_URL: '/api',
-    // BASE_URL: "http://localhost:8080/api",
+    BASE_URL: "http://localhost:8080/api",
     // BASE_URL: "http://192.168.1.81:8080/api",
     GUEST: "/guest",
     HOME: "/",
