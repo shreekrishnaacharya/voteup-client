bfc46ef307636088b36e8b5143f30c38c1e5c88a
shreekrishnaacharya <sksharma72000@gmail.com>
2023-01-15 19:54:13 +0545

    adding ssl point

--- src/links/pages.jsx
+++ src/links/pages.jsx
@@ -1,64 +1,71 @@
 const votePages = {
-    FEEDS: "/vote/feeds",
-    POST: "/post",
-    USER: "/user",
-    PROFILE: "/profile"
-}
+  FEEDS: "/vote/feeds",
+  POST: "/post",
+  USER: "/user",
+  PROFILE: "/profile",
+};
 
 const sitePage = {
-    LOGIN: "/guest",
-    NEW_PASSWORD: "/guest/new-password",
-    VERIFY_ACCOUNT: "/guest/verify-account",
-    SIGNUP: "/guest/signup",
-    FORGOT_PASSWORD: "/guest/forgot-password",
-    VERIFY_TOKEN: "/guest/verify-token",
-}
+  LOGIN: "/guest",
+  NEW_PASSWORD: "/guest/new-password",
+  VERIFY_ACCOUNT: "/guest/verify-account",
+  SIGNUP: "/guest/signup",
+  FORGOT_PASSWORD: "/guest/forgot-password",
+  VERIFY_TOKEN: "/guest/verify-token",
+};
 const otherPages = {
-    PAGES: "/page",
-    STATUS_404: "/page/404",
-    PRIVACY: "/page/privacy-policy",
-    TERMS: "/page/terms-and-conditions",
-    FAQ: "/page/faq",
-    RULES: "/page/rules",
-    BLOG: "/page/blog",
-    ABOUT: "/page/about",
-}
+  PAGES: "/page",
+  STATUS_404: "/page/404",
+  PRIVACY: "/page/privacy-policy",
+  TERMS: "/page/terms-and-conditions",
+  FAQ: "/page/faq",
+  RULES: "/page/rules",
+  BLOG: "/page/blog",
+  ABOUT: "/page/about",
+};
 
 const pages = {
-    BASE_URL: "http://ventvoila.com/api",
-    // BASE_URL: '/api',
-    // BASE_URL: "http://localhost:3000/api",
-    // BASE_URL: "http://localhost:8080/api",
-    GUEST: "/guest",
-    HOME: "/",
-    ...otherPages,
-    ...sitePage,
-    ...votePages
+  // BASE_URL: "http://ventvoila.com/api",
+  // BASE_URL: '/api',
+  BASE_URL: "https://referendum.ktm.yetiappcloud.com/api",
+  // BASE_URL: "http://localhost:8080/api",
+  GUEST: "/guest",
+  HOME: "/",
+  ...otherPages,
+  ...sitePage,
+  ...votePages,f
 };
 
 const guestPage = [
-    pages.FORGOT_PASSWORD,
-    pages.LOGIN,
-    pages.SIGNUP,
-    pages.NEW_PASSWORD,
-    pages.VERIFY_ACCOUNT,
-    pages.VERIFY_TOKEN,
-
+  pages.FORGOT_PASSWORD,
+  pages.LOGIN,
+  pages.SIGNUP,
+  pages.NEW_PASSWORD,
+  pages.VERIFY_ACCOUNT,
+  pages.VERIFY_TOKEN,
 ];
 
 const staticPages = [
-    otherPages.RULES,
-    otherPages.BLOG,
-    otherPages.FAQ,
-    otherPages.PAGES,
-    otherPages.PRIVACY,
-    otherPages.TERMS,
-    otherPages.ABOUT,
-    otherPages.STATUS_404
-]
+  otherPages.RULES,
+  otherPages.BLOG,
+  otherPages.FAQ,
+  otherPages.PAGES,
+  otherPages.PRIVACY,
+  otherPages.TERMS,
+  otherPages.ABOUT,
+  otherPages.STATUS_404,
+];
 
 const getFullUrl = (page) => {
-    return pages.LOCAL_URL + "#" + page;
-}
+  return pages.LOCAL_URL + "#" + page;
+};
 
-export { pages, sitePage, votePages, guestPage, otherPages, staticPages, getFullUrl };
+export {
+  pages,
+  sitePage,
+  votePages,
+  guestPage,
+  otherPages,
+  staticPages,
+  getFullUrl,
+};
