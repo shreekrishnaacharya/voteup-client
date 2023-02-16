7187e2600ea354b944166ed230cc01b9bf8a0304
shreekrishnaacharya <sksharma72000@gmail.com>
2023-02-16 17:01:57 +0545

    asas

--- src/links/pages.jsx
+++ src/links/pages.jsx
@@ -26,9 +26,9 @@ const otherPages = {
 
 const pages = {
   // BASE_URL: "http://ventvoila.com/api",
-  // BASE_URL: '/api',
+  BASE_URL: '/api',
   // BASE_URL: "https://referendum.ktm.yetiappcloud.com/api",
-  BASE_URL: "http://localhost:8080/api",
+  // BASE_URL: "http://localhost:8080/api",
   GUEST: "/guest",
   HOME: "/",
   ...otherPages,
